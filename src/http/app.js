afa9801956190768e922f43e8915fe291226ac53
Alexandros Koufatzis <alexk@Alexandross-MacBook-Pro-2.local>
2017-09-12 17:01:38 +0200

    Add body parser to parse json bodies. Ignore dist folder

--- src/http/app.js
+++ src/http/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const userRoute = require('./routes/user');
 const errorRoute = require('./routes/error');
 
 const app = express();
+app.use(bodyParser.json());
 
 module.exports = (services) => {
   const user = userRoute.create(services);
