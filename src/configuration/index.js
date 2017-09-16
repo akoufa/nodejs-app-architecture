3591fc226f8230777759886a4a1a32026b37ad81
Alexandros <akoufa@gmail.com>
2017-09-16 20:47:58 +0200

    Add documention to the configuration module

--- src/configuration/index.js
+++ src/configuration/index.js
@@ -1,3 +1,7 @@
+/**
+ * This module is used to collect all the configuration variables,
+ * like the environment vars, in one place so they are not scattered all over the whole codebase
+ */
 const config = {
   connectionString: process.env.DATABASE_CONNECTION_STRING,
   port: process.env.PORT || 5000,
