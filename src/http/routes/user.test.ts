40eed3f93dd5082f2d33d2a1fdb48fe47154fdc9
Alex Koufatzis <4354310+akoufatzis@users.noreply.github.com>
2018-10-08 23:55:40 +0200

    Add jest test

---
+++ src/http/routes/user.test.ts
@@ -0,0 +1,58 @@
+import request from 'supertest';
+import { appFactory } from '../../http/app';
+import { UserService } from '../../services/userService';
+import { UserRepository } from '../../repositories/userRepository';
+
+const mockGetAll = jest.fn();
+const mockAdd = jest.fn();
+
+jest.mock('../../repositories/userRepository.ts', () => ({
+  UserRepository: jest.fn().mockImplementation(() => {
+    return {
+      getAll: mockGetAll,
+      add: mockAdd,
+    };
+  }),
+}));
+
+const userData = [
+  { name: 'Alex', age: 30 },
+  { name: 'Aris', age: 29 },
+  { name: 'Pantelis', age: 40 },
+];
+
+const userRepository = new UserRepository();
+const userService = new UserService(userRepository);
+
+const app = appFactory(userService);
+
+describe('user route test', () => {
+  describe('GET /users test', () => {
+    beforeEach(() => {
+      (UserRepository as any).mockClear();
+      mockGetAll.mockClear();
+      mockAdd.mockClear();
+    });
+
+    it('should return 200 an array of users', async () => {
+      mockGetAll.mockResolvedValue(userData);
+
+      const { body: users } = await request(app)
+        .get('/users')
+        .expect(200);
+
+      expect(users).toEqual(userData);
+    });
+
+    it('should return 500 when the service rejects with an error', () => {
+      const dbError = new Error('Database error');
+      mockGetAll.mockRejectedValue(dbError);
+      return request(app)
+        .get('/users')
+        .expect(500)
+        .catch(error => {
+          expect(error).toEqual(dbError);
+        });
+    });
+  });
+});
